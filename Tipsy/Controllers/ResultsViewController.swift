575f016fb2da5f6662efca16e36733f5d12927af
Islam Nurmukhametov <islam.nurmuhametov@omega-r.com>
2023-08-16 23:05:00 +0300

    completed challenge

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Ислам Нурмухаметов on 16.08.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    var result = "0.0"
+    var tip = 10
+    var split = 2
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        totalLabel.text = result
+        settingsLabel.text = "Split between \(split) people, with \(tip)% tip."
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
